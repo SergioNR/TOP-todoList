dd723a6ba7d736e21cf322f5eaec57866d0f2791
Sergio Navarro <s.navarroredondo@gmail.com>
2024-05-06 15:39:58 +0000

    UPDATE index.js dependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,12 @@
-import { displayTodos } from "./displayTodos.js";
-import { createDummyData } from "./createDummyData.js";
-import { TodoItem } from "./TodoItem.js";
-import "../css/styles.css";
-import { deleteAllTodos } from "./deleteAllTodos.js";
-import { cycleTodoItemPriority } from "./cyclePriority.js";
-import { newTodoForm } from "./newTodoForm.js";
+import { todoList } from "./todoList.js";
+
+import { createNewTodoForm } from "./createNewTodoForm.js"
 
 
-export let todoList = [];
 
-todoList.push(
-  new TodoItem(
-    `Title 1`,
-    `Description 1`,
-    `Due date 1`,
-    `Low`,
-    `Notes 1`,
-    `Project 1`,
-    true,
-  ),
-);
-
-createDummyData(todoList);
-displayTodos(todoList);
-console.log(todoList)
-
-
+// createDummyData(todoList);
+// displayTodos(todoList);
+// console.log(todoList);
 
 // instead of saving on an array, we want to save the items onto local storage
 
@@ -39,8 +20,3 @@ What we can do is only use local storage?
 2nd read items from local storage
 3rd delete items from local storage
 */
-
-
-
-
-
