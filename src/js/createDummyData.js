844ecaaf6d34125840e59e5b38ff29398624c8c4
Sergio Navarro <s.navarroredondo@gmail.com>
2024-05-25 22:56:37 +0200

    ADD logic to dynamically generate todo data

--- src/js/createDummyData.js
+++ src/js/createDummyData.js
@@ -1,6 +1,5 @@
 import { todoList } from "./todoList.js";
-import { TodoItem } from "./TodoItemClass.js";
-import { pushTodoListToLocalStorage } from "./createNewTodoForm.js";
+import { createNewTodo, pushTodoListToLocalStorage } from "./createNewTodoForm.js";
 
 let createDummyDataButton = document.querySelector(".createDummyDataButton");
 
@@ -18,43 +17,48 @@ export const createDummyData = (todoList) => {
 
   } else {
 
-    const todoTitle = generateTodoTitle();
     
-    const todoDescription = generateTodoDescription();
-
     for (let i = 0; i < dummyTodoItems; i++) {
-      todoList.push(
-        new TodoItem(
-          `hello`,
-          `this is the description`,
-          `2024-12-31`,
-          `high`,
-          `this is the notes`,
-          `dummy Data`,
-          true,
-        ),
-      );
+      
+      const todoTitle = generateTodoTitle();
+      
+      const todoDescription = generateTodoDescription();
+      
+      const todoDueDate = generateTodoDate();
+
+      const todoPriority = generateTodoPriority();
+
+      const todoNotes = generateTodoNotes();
+
+      const todoProject = generateTodoProject();
+
+      const todoStatus = generateTodoStatus();
+      
+      let todoItem = createNewTodo(todoTitle, todoDescription, todoDueDate, todoPriority, todoNotes, todoProject, todoStatus);
+      
+      todoList.push(todoItem)
+      
     }
   }
 
   pushTodoListToLocalStorage(todoList);
 };
 
-generateTodoTitle = () => {
+const generateTodoTitle = () => {
   let randomWords = [
-  `Organize`
-  `Exercise`
-  `Budgeting`
-  `Save`
-  `Mediate`
-  `Read`
-  `Connect`
-  `Learn`
-  `Declutter`
-  `Gratitude`
-  `Create`
-  `Rest`
-  `Plan`
+  `Organize`,
+  `Exercise`,
+  `Budgeting`,
+  `Save`,
+  `Mediate`,
+  `Read`,
+  `Connect`,
+  `Learn`,
+  `Declutter`,
+  `Gratitude`,
+  `Create`,
+  `Rest`,
+  `Plan`,
   `Reflect`
   ];
 
@@ -63,7 +67,7 @@ generateTodoTitle = () => {
   return randomTitle;
 }
 
-generateTodoDescription = () => {
+const generateTodoDescription = () => {
   const descriptions = [
     'A brief overview of the history of the internet and its impact on modern society.',
     'An introduction to the principles of object-oriented programming and its applications.',
@@ -75,8 +79,51 @@ generateTodoDescription = () => {
   const randomIndex = Math.floor(Math.random() * descriptions.length);
   
   return descriptions[randomIndex];
-
 }
 
 
+const generateTodoDate = () => {
+  let dueDate = new Date();
 
+  dueDate.setDate(dueDate.getDate() + Math.floor(Math.random() * 7));
+
+  return dueDate.toISOString().split('T')[0];
+}
+
+const generateTodoPriority = () => {
+  let priorities = ['low', 'medium', 'high'];
+
+  let randomPriority = priorities[Math.floor(Math.random() * priorities.length)]
+
+  return randomPriority
+}
+
+const generateTodoNotes = () => {
+  let notes = [`Check inventory`,
+    `Follow up email`,
+    `Submit proposal`,
+    `Pay bills online`,
+    `Finish report`,
+    `Schedule meeting`,
+    `Call client`,
+    `Clean office space`,
+    `Buy groceries`,
+    `File tax return`]
+
+    let randomNote = notes[Math.floor(Math.random() * notes.length)]
+
+    return randomNote;
+}
+
+const generateTodoProject = () => {
+
+}
+
+const generateTodoStatus = () => {
+
+  let statuses = [true, false];
+
+  let randomStatus = statuses[Math.floor(Math.random() * statuses.length)]
+
+  return randomStatus
+}
