5f2b4ca8730e443ded74d5bd54b15b1f0c0b4342
Sergio Navarro <s.navarroredondo@gmail.com>
2024-05-25 18:47:49 +0200

    DELETE deleteTodoItem.js file
    Upgraded to new & better code

--- src/js/P-deleteTodoItem.js
+++
@@ -1,4 +0,0 @@
-// export const deleteTodoButton = document.createElement('button');
-//     deleteTodoButton.classList.add('deleteTodoButton');
-//     deleteTodoButton.innerText = 'Delete To-Do';
-//     todoCard.appendChild(deleteTodoButton);
