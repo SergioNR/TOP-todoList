ac5b8b4283ce67d60d1062568e096f3c0367644d
Sergio Navarro <s.navarroredondo@gmail.com>
2024-05-31 21:49:03 +0200

    Storing local changes before reverting

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,29 @@ import { deleteAllTodos } from "./deleteAllTodos.js";
 
 import { createDummyData } from "./createDummyData.js";
 
+// Create a hidden input element
+let input = document.createElement('input');
+input.type = 'hidden';
+input.id = 'storage';
+document.body.appendChild(input);
+
+// Initialize the input value with localStorage value
+input.value = localStorage.getItem('todoList');
+
+// Create a MutationObserver instance
+let observer = new MutationObserver(function() {
+  // When the input value changes, update the localStorage
+  localStorage.setItem('todoList', input.value);
+});
+
+// Start observing the input element for attribute changes
+observer.observe(input, { attributes: true });
+
+// Whenever you want to change the localStorage,
+// instead of using localStorage.setItem,
+// you change the input value
+input.value = 'new value';
+
 displayTodos()
 
 // instead of saving on an array, we want to save the items onto local storage
